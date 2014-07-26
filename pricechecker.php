13b52bce27c74c4e44b8efe52073294efecbd5d8
Austin Heiman <atheimanksu@gmail.com>
2014-07-26 15:38:21 -0500

    pricechecker php working, frontend started

--- pricechecker.php
+++ pricechecker.php
@@ -1,44 +1,40 @@
 <?php
 
-if (!isset($_GET['itemid']) or $_GET['itemid'] == "") {
-	die("ERROR - No 'itemid' specified in GET paramaters. Expected 10 character alphanumeric string.");
+
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+
+
+if (!isset($_GET['itemId']) or $_GET['itemId'] == "") {
+	die("ERROR - No 'itemId' specified in GET paramaters. Expected 10 character alphanumeric string.");
 }
 
-// Save itemID after cleaning any malicious input
-$itemID = htmlspecialchars($_GET['itemid']);
+// Save itemId after cleaning any malicious input
+$itemId = htmlspecialchars($_GET['itemId']);
 
 // Check cacheDir for recent entry of the item first
 $cacheDir = "cache";
 
-/*
-if cacheDir exists {
-	if file named itemID exists {
-		if datemodified is within last x seconds {
-			read file, set itemPrice and itemName
-		}
-	}
-	file is too old or doesnt exist, so clear it
-} else {
-	create dir
-}
-*/
-
-if (file_exists($cacheDir)) {
-	if (file_exists($cacheDir."/".$itemID)) {
-		$dateStrModified = filemtime($cacheDir."/".$itemID);
+if (file_exists($cacheDir)) {  // if cacheDir exist
+	if (file_exists($cacheDir."/".$itemId)) {  // if file named itemId exists
+		$dateStrModified = filemtime($cacheDir."/".$itemId);
 		$timeTooOldInSec = 1;
-		if ((time() - $timeTooOldInSec) < $dateStrModified) {
+		if ((time() - $timeTooOldInSec) < $dateStrModified) {  // if datemodified is within last x seconds
 			// file last modified recent enough, return values from the file
 			die("file found, return values from file here.");
+			// TODO Read in id, name, price from cache file
 		}
 	}
 	// file doesnt exist or is too old to be useful. Either way, clear it
-	file_put_contents($cacheDir."/".$itemID , "");
+	file_put_contents($cacheDir."/".$itemId , "");
 } else {
+	// cacheDir doesn't exist yet, create it so we can start caching amazon searches
 	mkdir ($cacheDir, 0755);
 }
 
-$amazon_url = "http://www.amazon.com/dp/" . $itemID;
+// Couldnt find cached information for this product, search amazon.com
+
+$amazon_url = "http://www.amazon.com/dp/" . $itemId;
 
 $curl = curl_init();
 
@@ -84,13 +80,13 @@ if($httpCode == 404) {
 	//echo "itemName:<br>";var_dump($itemName);echo "<br><br>";
 }
 
-// Write the item id, item name, and item price to the cache file (named itemID)
-file_put_contents($cacheDir."/".$itemID , $itemID . "\n" . $itemName . "\n" . $itemPrice);
+// Write the item Id, item name, and item price to the cache file (named itemId)
+file_put_contents($cacheDir."/".$itemId , $itemId . "\n" . $itemName . "\n" . $itemPrice);
 
 ?>
 
 {
-	"itemID" : "<?php echo $itemID?>",
+	"itemId" : "<?php echo $itemId?>",
 	"itemName" : "<?php echo $itemName?>",
 	"itemPrice" : "<?php echo $itemPrice?>"
 }
